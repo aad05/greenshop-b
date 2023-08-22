7af99fa79fd3dc596baa609b0322acfe1df31d36
aad05 <abduvoitovasadbek3@gmail.com>
2023-08-22 09:32:22 +0500

    wishlist

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,25 @@
 const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require("uuid");
 const { errorStatus500, bodyRequirer } = require("../errors");
 const { user } = require("../models/userModel");
+const {
+  house_plants,
+  potter_plants,
+  seeds,
+  small_plants,
+  big_plants,
+  succulents,
+  trerrariums,
+  gardening,
+  accessories,
+} = require("../models/flowers/flowerModel");
 
 // SignIn required values...
 const sign_in_required_values = ["password", "email"];
 const sign_up_required_values = ["name", "surname", "password", "email"];
+
+// Wishlist required values...
+const wishlist_required_values = ["route_path", "flower_id"];
 
 // Default value
 const default_permission = {
@@ -104,11 +119,102 @@ const sign_up = async ({ body, query }, res) => {
   }
 };
 
-// Method: POST: Description: User sign in as a chief user
-const chief_user_sign_in = async ({ body }, res) => {
+const get_by_id = async ({ type, _id }) => {
+  switch (type) {
+    case "house-plants":
+      return await house_plants.findById(_id);
+    case "potter-plants":
+      return await potter_plants.findById(_id);
+    case "seeds":
+      return await seeds.findById(_id);
+    case "small_plants":
+      return await small_plants.findById(_id);
+    case "big_plants":
+      return await big_plants.findById(_id);
+    case "succulents":
+      return await succulents.findById(_id);
+    case "trerrariums":
+      return await trerrariums.findById(_id);
+    case "gardening":
+      return await gardening.findById(_id);
+    case "accessories":
+      return await accessories.findById(_id);
+    default:
+      throw new Error("Request should be provided with plant category!");
+  }
+};
+
+// Method: Get: Description: Get wishlist of user
+const get_wishlist = async ({ query }, res) => {
   try {
+    const { access_token } = query;
+
+    const foundUser = await user.findById(access_token);
+    const data = [];
+    for await (const value of foundUser.wishlist)
+      data.push(
+        await get_by_id({
+          type: value.route_path,
+          _id: value.flower_id,
+        })
+      );
+
+    return res.status(200).json({
+      message: "success",
+      data,
+    });
   } catch (error) {
     return errorStatus500(error, res);
   }
 };
-module.exports = { sign_in, sign_up };
+
+// Method: POST; Description: Add wishlist
+const create_wishlist = async ({ body, query: { access_token } }, res) => {
+  try {
+    await bodyRequirer({ body, requiredValue: wishlist_required_values });
+    const foundUser = await user.findById(access_token);
+    await user.findByIdAndUpdate(access_token, {
+      ...foundUser._doc,
+      wishlist: [
+        ...foundUser.wishlist,
+        {
+          route_path: body.route_path,
+          flower_id: body.flower_id,
+          _id: uuidv4(),
+        },
+      ],
+    });
+
+    return res.status(201).json({
+      message: "success",
+    });
+  } catch (error) {
+    return errorStatus500(error, res);
+  }
+};
+
+// Method: DELETE; Descripition: Delete wished flower
+const delete_wishlist = async ({ body, query: { access_token } }, res) => {
+  try {
+    await bodyRequirer({ body, requiredValue: ["_id"] });
+    const foundUser = await user.findById(access_token);
+    await user.findByIdAndUpdate(access_token, {
+      ...foundUser._doc,
+      wishlist: foundUser.wishlist.filter((value) => value._id !== body._id),
+    });
+
+    return res.status(201).json({
+      message: "success",
+    });
+  } catch (error) {
+    return errorStatus500(error, res);
+  }
+};
+
+module.exports = {
+  sign_in,
+  sign_up,
+  get_wishlist,
+  create_wishlist,
+  delete_wishlist,
+};
--- routes/user_route.js
+++ routes/user_route.js
@@ -1,12 +1,29 @@
 const { Router } = require("express");
-const { sign_in, sign_up } = require("../controller/userController");
+const {
+  sign_in,
+  sign_up,
+  get_wishlist,
+  create_wishlist,
+  delete_wishlist,
+} = require("../controller/userController");
 const permissionChecker = require("../tools/permissionChecker");
 const verifyToken = require("../tools/tokenVerification");
-const { make_order } = require("../controller/orderController");
 const router = Router();
 
 router.post("/sign-in", permissionChecker, sign_in);
 router.post("/sign-up", permissionChecker, verifyToken, sign_up);
-router.post("/make-order", permissionChecker, verifyToken, make_order);
+router.get("/wishlist", permissionChecker, verifyToken, get_wishlist);
+router.post(
+  "/create-wishlist",
+  permissionChecker,
+  verifyToken,
+  create_wishlist
+);
+router.delete(
+  "/delete-wishlist",
+  permissionChecker,
+  verifyToken,
+  delete_wishlist
+);
 
 module.exports = router;
